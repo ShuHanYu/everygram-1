cc6746a13a305b62d4bcc4ecf157d9ffc47cae88
leeboyin <leeboyin945@gmail.com>
2020-05-22 16:12:32 +0800

    add mode to demo login

--- src/demo/DemoLogin.vue
+++ src/demo/DemoLogin.vue
@@ -10,7 +10,7 @@
 				</div>
 				<div class="col-md-8 col-lg-6 col-xl-5 offset-xl-1 mx-auto">
 					<!-- Login -->
-					<div class="login__form">
+					<div v-if="mode === 'signIn'" class="login__form">
 						<a href="/#/demo/home" class="login__logo mb-5">
 							<img src="static/images/logo-vertical.svg" alt="">
 						</a>
@@ -30,9 +30,9 @@
 							<MdcButton class="mdc-button--raised w-100">登入</MdcButton>
 						</div>
 						<div class="text-center mb-2">
-							<MdcButton class="mdc-button--text">註冊會員</MdcButton>
+							<MdcButton class="mdc-button--text" @click.native="mode = 'signUp'">註冊會員</MdcButton>
 							<span class="text-muted">|</span>
-							<MdcButton class="mdc-button--text">忘記密碼</MdcButton>
+							<MdcButton class="mdc-button--text" @click.native="mode = 'forgetPassword'">忘記密碼</MdcButton>
 						</div>
 						<div class="mb-3 text-center text-muted">或</div>
 						<div>
@@ -44,7 +44,7 @@
 					</div>
 
 					<!-- Sign Up -->
-					<!-- <div class="login__form">
+					<div v-if="mode === 'signUp'" class="login__form">
 						<div class="mb-4">
 							<MdcTextField
 								type="text"
@@ -64,18 +64,18 @@
 							/>
 						</div>
 						<div class="mb-2">
-							<MdcButton class="mdc-button--raised w-100">註冊</MdcButton>
+							<MdcButton class="mdc-button--raised w-100" @click.native="mode = 'singUpSuccess'">註冊</MdcButton>
 						</div>
 						<div>
-							<MdcButton class="mdc-button--text">
+							<MdcButton class="mdc-button--text" @click.native="mode = 'signIn'">
 								<i slot="icon" class="material-icons material-icons-outlined mdc-button__icon">keyboard_arrow_left</i>
 								返回登入
 							</MdcButton>
 						</div>
-					</div> -->
+					</div>
 
 					<!-- Forgot Password -->
-					<!-- <div class="login__form">
+					<div v-if="mode === 'forgetPassword'" class="login__form">
 						<div class="mb-4">
 							<MdcTextField
 								type="email"
@@ -86,15 +86,15 @@
 							<MdcButton class="mdc-button--raised w-100">寄送密碼重設信件</MdcButton>
 						</div>
 						<div>
-							<MdcButton class="mdc-button--text">
+							<MdcButton class="mdc-button--text" @click.native="mode = 'signIn'">
 								<i slot="icon" class="material-icons material-icons-outlined mdc-button__icon">keyboard_arrow_left</i>
 								返回登入
 							</MdcButton>
 						</div>
-					</div> -->
+					</div>
 
 					<!-- 註冊成功 -->
-					<!-- <div class="login__form">
+					<div v-if="mode === 'singUpSuccess'" class="login__form">
 						<h3>註冊成功！</h3>
 						<p>請先至電子信箱點選驗證連結以完成註冊流程</p>
 						<div class="mb-4">
@@ -102,12 +102,12 @@
 							<MdcButton class="mdc-button--text">重新發送</MdcButton>
 						</div>
 						<div>
-							<MdcButton class="mdc-button--text">
+							<MdcButton class="mdc-button--text" @click.native="mode = 'signIn'">
 								<i slot="icon" class="material-icons material-icons-outlined mdc-button__icon">keyboard_arrow_left</i>
 								返回登入
 							</MdcButton>
 						</div>
-					</div> -->
+					</div>
 				</div>
 			</div>
 		</div>
@@ -122,6 +122,11 @@ export default {
 	components: {
 		MdcButton,
 		MdcTextField,
+	},
+	data() {
+		return {
+			mode: 'signIn',
+		};
 	},
 };
 </script>
